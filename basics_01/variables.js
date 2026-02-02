791d80dd67667aced98fc7b786a0ab7958c5fb6b
Igave Mangesh Digambar <mangeshigave8799@gmail.com>
2026-02-03 00:02:09 +0530

    in this we use a variables like var , const , let and also use console.table for tabular form data.

---
+++ basics_01/variables.js
@@ -0,0 +1,12 @@
+const accountId = 112233
+let accountEmail = "mangeshigave@email.com"
+var accountPassword = "4321"
+accountCity = "jaipur"
+
+accountEmail = "amoligave@gmail.com"
+accountPassword ="5432"
+accountCity = "Latur"
+
+console.log(accountEmail);
+console.table([accountEmail , accountId , accountPassword , accountCity])
+ 
