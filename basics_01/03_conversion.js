39c849446b11719d2cd0605bbfe543b921892c48
Igave Mangesh Digambar <mangeshigave8799@gmail.com>
2026-02-03 23:40:39 +0530

    Create 03_conversion.js
    datatypes conversion

---
+++ basics_01/03_conversion.js
@@ -0,0 +1,13 @@
+let score ="33" ;
+console.log(typeof score);
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn =true ;
+console.log(typeof isLoggedIn);
+
+let valueString = String(isLoggedIn)
+console.log(typeof valueString);
+console.log(valueString);
