f836aaf3c9eb04a7ab338e336845681de606863a
Igave Mangesh Digambar <mangeshigave8799@gmail.com>
2026-02-03 22:55:32 +0530

    Update 02_datatypes.js
    some conversions and date types

--- basics_01/02_datatypes.js
+++ basics_01/02_datatypes.js
@@ -1,11 +1,15 @@
 let name ="mangesh"
 let x=10;
 let isLoggedIn = true ;
-let m;
+let m;  //undefined
 let big =1234567895322456324;
 
 
 console.log(x);
 console.log(m);
+console.log(typeof true);
+console.log(typeof "mangesh");
+
+
 
 
